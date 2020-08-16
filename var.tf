744ea81f7329e95c744336681e4ce3e43acd344c
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-16 18:21:16 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,8 +2,8 @@ variable "ami" {
   type = "map"
 
   default = {
-    "us-east-1" = "ami-04169656fea786776"
-    "us-west-1" = "ami-006fce2a9625b177f"
+    "us-east-1" = "ami-03dbf9550d4620230"
+    "us-west-1" = "ami-03dbf9550d4620231"
   }
 }
 
