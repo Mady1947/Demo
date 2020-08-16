1f7b416626471de4aa7aed446f9a90ae0cdd2d2d
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-16 19:27:24 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -2,7 +2,7 @@ variable "subnet_ids" {
   type = list(string)
 }
 
-resource "aws_instance" "server" {
+resource "aws_instance" "Test" {
   # Create one instance for each subnet
   count = length(var.subnet_ids)
 
@@ -11,6 +11,6 @@ resource "aws_instance" "server" {
   subnet_id     = var.subnet_ids[count.index]
 
   tags = {
-    Name = "Server ${count.index}"
+    Name = "Test ${count.index}"
   }
 }
