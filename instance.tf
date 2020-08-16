a9fe2e3c73c85c30ba4c149240549aa2a256e301
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-16 20:37:33 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "swa" {
 
   ami           = "ami-03dbf9550d4620230"
   instance_type = "t2.micro"
+  subnet_ids    ="subnet-c488fd88"
 
   tags = {
     Name = "swa ${count.index}"
