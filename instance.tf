006dec8eb32b807568dcdc8366b1686819738f40
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-16 20:43:50 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,14 +4,14 @@
 }
 
 resource "aws_instance" "my-instance" {
-  count         = "${var.instance_count}"
-  ami           = "${lookup(var.ami,var.aws_region)}"
-  instance_type = "${var.instance_type}"
-  key_name      = "${aws_key_pair.terraform-demo.key_name}"
-  user_data     = "${file("install_apache.sh")}"
+  count         = var.instance_count
+  ami           = lookup(var.ami,var.aws_region)
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.terraform-demo.key_name
+  user_data     = file("install_apache.sh")
 
   tags = {
-    Name  = "${element(var.instance_tags, count.index)}"
+    Name  = element(var.instance_tags, count.index)
     Batch = "5AM"
   }
 }
