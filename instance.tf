6182fb868b4c29a426015207b8b74fd253374f4c
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-16 20:39:50 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,11 +1,17 @@
-resource "aws_instance" "swa" {
-  count = 2 # create four similar EC2 instances
+ resource "aws_key_pair" "terraform-demo" {
+  key_name   = "terraform-demo"
+  public_key = "${file("terraform-demo.pub")}"
+}
 
-  ami           = "ami-03dbf9550d4620230"
-  instance_type = "t2.micro"
-  subnet_ids    ="subnet-c488fd88"
+resource "aws_instance" "my-instance" {
+  count         = "${var.instance_count}"
+  ami           = "${lookup(var.ami,var.aws_region)}"
+  instance_type = "${var.instance_type}"
+  key_name      = "${aws_key_pair.terraform-demo.key_name}"
+  user_data     = "${file("install_apache.sh")}"
 
   tags = {
-    Name = "swa ${count.index}"
+    Name  = "${element(var.instance_tags, count.index)}"
+    Batch = "5AM"
   }
 }
