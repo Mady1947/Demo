bebda2534a2822a27150aa1b978ca0f3506667c9
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-16 19:29:18 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "Test" {
+resource "aws_instance" "swa" {
   count = 2 # create four similar EC2 instances
 
   ami           = "ami-03dbf9550d4620230"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Test ${count.index}"
+    Name = "swa ${count.index}"
   }
 }
