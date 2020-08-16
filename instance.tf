1e61831e359e66b334fbffa4f0cfc7c32726fd6c
Mady1947 <63644681+Mady1947@users.noreply.github.com>
2020-08-16 22:18:23 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,3 +1,4 @@
+provider "aws" { region = "us-west-2" access_key = "my-access-key" secret_key = "my-secret-key" }
 resource "aws_key_pair" "terraform-demo" {
   key_name   = "terraform-demo"
   public_key = file("terraform-demo.pub")
